ba086a1ccada16b57c80311f56b554e327359500
Elias F Martins <eliasmartinsdev8@gmail.com>
2025-02-26 08:54:31 -0300

    feat:criando outras pages

---
+++ src/app/discussoes/page.tsx
@@ -0,0 +1,7 @@
+export default function Categorias() {
+	return (
+		<div className="bg-[#0F172A] flex-1 flex flex-col">
+			<main className="flex-1 flex flex-col items-center justify-center text-center px-6"></main>
+		</div>
+	)
+}
